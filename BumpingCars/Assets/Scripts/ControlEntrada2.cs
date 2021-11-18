a79fda2bcd31037cbd855a454fa0415c72d3a52f
DanielMMITM <19121051@morelia.tecnm.mx>
2021-11-18 11:39:18 -0600

    Multiplayer Local
    -Se agrego lo necesario para que el juego pueda ser entre dos personas en un equipo local

---
+++ BumpingCars/Assets/Scripts/ControlEntrada2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEntrada2 : MonoBehaviour
+{
+    //Componentes
+    Driver driver2;
+
+    //Awake is called when the script instance is being loaded
+    void Awake() {
+        driver2 = GetComponent<Driver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.I) || Input.GetKey(KeyCode.J) || Input.GetKey(KeyCode.K) || Input.GetKey(KeyCode.L)){
+            Vector2 vectorEntrada2 = Vector3.zero;
+
+            vectorEntrada2.x = Input.GetAxis("Horizontal2");
+            vectorEntrada2.y = Input.GetAxis("Vertical2")*-1;
+
+            driver2.SetVectorEntrada(vectorEntrada2);
+        //}
+    }
+
+}
